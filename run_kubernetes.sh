6ed527ba113531d245a4e0c1b137779850deb515
bmokase3093 <bmokase3093@gmail.com>
2022-09-01 16:35:28 +0000

    project files

--- run_kubernetes.sh
+++ run_kubernetes.sh
@@ -4,15 +4,19 @@
 
 # Step 1:
 # This is your Docker ID/path
-# dockerpath=<>
+dockerpath=bmokas3093/mlproject:v1.0
 
 # Step 2
 # Run the Docker Hub container with kubernetes
-
+kubectl run mlproject \
+    --image=bmokas3093/mlproject:v1.0 --port=80 \
+    --labels app=mlproject
 
 # Step 3:
 # List kubernetes pods
+kubectl get pods
 
 # Step 4:
 # Forward the container port to a host
+kubectl port-forward mlproject 8000:80
 
